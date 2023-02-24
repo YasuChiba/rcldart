8dfb5123580e072878065d75ba99c427b882fa3d
YasuChiba <yasu.bv0309@gmail.com>
2023-02-25 02:30:14 +0900

    init

---
+++ example/rcldart_example.dart
@@ -0,0 +1,6 @@
+import 'package:rcldart/rcldart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/rcldart_test.dart
@@ -0,0 +1,16 @@
+import 'package:rcldart/rcldart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
