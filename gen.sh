ab5d037b554521ed7fcb88224cbea0164eff0544
YasuChiba <yasu.bv0309@gmail.com>
2023-02-28 03:04:10 +0900

    update ffigen

--- gen.sh
+++ gen.sh
@@ -3,4 +3,4 @@ rm -rf ffigen/src
 mkdir ffigen/src
 
 vcs import --input ffigen/ros2-flutter.repos ffigen/src/
-flutter pub run ffigen --config ffigen/ffigen_rcl.yaml
+dart run ffigen --config ffigen/ffigen_rcl.yaml
